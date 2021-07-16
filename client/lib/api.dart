dc94a67f3af700a717785f72962ebfa7255df12d
serpest <serpest@protonmail.com>
2021-07-16 21:37:54 +0200

    Specify API URL in client when debugging or building the application

--- client/lib/api.dart
+++ client/lib/api.dart
@@ -3,8 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:tidraw/model/draw.dart';
 
+// API_URL has to be specified when debugging or building the application using the argument "--dart-define=API_URL=<http://example.com>"
+const String API_URL = String.fromEnvironment('API_URL');
+
 Future<Draw> getDraw(String id) async {
-  final response = await http.get(Uri.parse('http://localhost:8080/api/draws/' + id));
+  final response = await http.get(Uri.parse(API_URL + '/api/draws/' + id));
   if (response.statusCode == 200) {
     return Draw.fromJson(jsonDecode(response.body));
   } else {
@@ -15,7 +18,7 @@ Future<Draw> getDraw(String id) async {
 
 Future<Draw> putDraw(Draw draw) async {
   final response = await http.post(
-    Uri.parse('http://localhost:8080/api/draws'),
+    Uri.parse(API_URL + '/api/draws'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
